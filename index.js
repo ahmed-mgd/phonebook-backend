776bbe614b444fda72c2929e6d1b3647df0a61f2
ahmed-mgd <ahmedg.abdel@gmail.com>
2024-07-17 17:02:29 -0400

    Deploy

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token("person", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
@@ -86,7 +88,7 @@ app.post("/api/persons/", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
