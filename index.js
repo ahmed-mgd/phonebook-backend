2643ad5e27b58542c4ff1da0530d8e5b2596b142
ahmed-mgd <ahmedg.abdel@gmail.com>
2024-07-17 18:02:20 -0400

    Finish part3b

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'))
 
 morgan.token("person", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
