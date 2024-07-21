2366915df88ec7897d91f975fec0a5cd68776524
ahmed-mgd <ahmedg.abdel@gmail.com>
2024-07-21 14:23:03 -0400

    Complete 3.14

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
 morgan.token("person", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
 });
 app.use(morgan(":method :url :response-time :person"));
-
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 app.get("/info", (req, res) => {
   const d = new Date();
@@ -45,18 +23,20 @@ app.get("/info", (req, res) => {
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(id).then((person) => {
+    if (person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -69,27 +49,28 @@ app.post("/api/persons/", (req, res) => {
   const newId = Math.floor(Math.random() * 200);
   const body = req.body;
 
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "missing name or number",
-    });
-  } else if (persons.some((person) => person.name === body.name)) {
-    return res.status(400).json({
-      error: "duplicate name",
-    });
-  }
+  // if (!body.name || !body.number) {
+  //   return res.status(400).json({
+  //     error: "missing name or number",
+  //   });
+  // } else if (Person.some((person) => person.name === body.name)) {
+  //   return res.status(400).json({
+  //     error: "duplicate name",
+  //   });
+  // }
 
-  const person = {
-    id: newId,
+  const person = new Person({
     name: body.name,
-    number: body.number,
-  };
-
-  persons = persons.concat(person);
+    number: body.number
+  });
+  
+  person.save().then((result) => {
+    console.log(`Added ${person.name} number ${person.number} to phonebook`);
+  });
   res.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
