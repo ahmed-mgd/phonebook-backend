222366784dcd62d15bafcea0de0ba8efe6c19588
ahmed-mgd <ahmedg.abdel@gmail.com>
2024-07-22 23:51:40 -0400

    Fix linter errors

--- models/person.js
+++ models/person.js
@@ -1,20 +1,20 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
@@ -24,17 +24,17 @@ const personSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: (v) => /^\d+-\d+$/.test(v),
-      message: (props) => `Enter a valid phone number in the form XX-XX...`,
+      message: () => 'Enter a valid phone number in the form XX-XX...',
     },
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
